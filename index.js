0a17e53c9e6a08d238c07501eb7c2f160017620c
Liam Hubers <liam.hubers@gmail.com>
2016-06-22 15:20:47 +0200

    Added tests for plugin

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const { setConfig, can, check } = require('flex-authorization');
 
+const getRoleKey = (roleKey) => {
+  return roleKey ? roleKey : 'role';
+}
+
+const setRole = (user, roleKey, role, params) => {
+  const userRole = typeof role === 'string' ? user[role] : role(user, params);
+  if (user) user[roleKey] = userRole;
+
+  return user;
+};
+
 const authorizationPlugin = {
   register: (server, options, next) => {
-    const roleKey = options.roleKey ? options.roleKey : 'role';
+    const roleKey = getRoleKey(options.roleKey);
     setConfig({ actions: options.actions, role: roleKey });
 
     server.ext('onPostAuth', (req, reply) => {
-      const user = req.auth.credentials;
-      user.set(roleKey, options.role(user, req.params));
+      try {
+        const user = setRole(req.auth.credentials, roleKey, options.role, req.params);
+        if (!user) return reply.continue();
 
-      reply.continue({
-        credentials: user,
-      });
+        reply.continue({ credentials: user });
+      } catch (err) {
+        reply.continue();
+      };
     });
 
     next();
@@ -19,10 +32,12 @@ const authorizationPlugin = {
 };
 
 authorizationPlugin.register.attributes = {
-  name: 'flex-hapi-authorization',
+  name: 'flex-hapi-authorization-plugin',
   version: '0.0.1',
 };
 
 module.exports = authorizationPlugin;
 module.exports.can = can;
 module.exports.check = check;
+module.exports.setRole = setRole;
+module.exports.getRoleKey = getRoleKey;
